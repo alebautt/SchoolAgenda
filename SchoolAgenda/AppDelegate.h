166434094eb8146e3fa8ef16cb35c8880a6cf9e2
alebautt <id_bautista@hotmail.com>
2015-03-10 22:59:10 -0600

    Initial Commit

---
+++ SchoolAgenda/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SchoolAgenda
+//
+//  Created by Alejandra B on 10/03/15.
+//  Copyright (c) 2015 alebautista. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SchoolAgenda/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SchoolAgenda
+//
+//  Created by Alejandra B on 10/03/15.
+//  Copyright (c) 2015 alebautista. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
